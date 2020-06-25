fb79508e105ae7ac30560a4378ead3ccc1e8695f
Michael Swain <swain@Michaels-MacBook-Pro-3.local>
2020-06-25 11:21:03 +0100

    add v9 directory

---
+++ v9/routes/comments.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const Campground = require("../models/campground");
+const Comment = require("../models/comment");
+
+
+// COMMENTS NEW
+router.get("/new", isLoggedIn, function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", { campground: campground });
+        }
+    });
+});
+
+// COMMENTS CREATE
+router.post("/", isLoggedIn, function (req, res) {
+    // lookup campground using ID
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            // create new comment
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    // add username and id to comment
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    // save comment
+                    comment.save();
+                    campground.comments.push(comment);
+                    campground.save();
+                    console.log(comment);
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            })
+            // connect new comment to campground
+            // redirect to campground show page
+        }
+    });
+});
+
+// MIDDLEWARE
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
